4537ba8c5f89eb5426abdba8baca0054a864ec82
Animesh <unknowndevilcoder@gmail.com>
2026-04-13 12:09:56 +0530

    Intial Commit for App.jsx in  Grey Shade Studios

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,23 @@ import React from 'react';
 import Hero from './components/Hero';
 import Perspective from './components/Perspective';
 import SignatureCollection from './components/SignatureCollection';
+import ServiceCanvas from './components/ServiceCanvas'; // Import it here
 
 function App() {
   return (
-    <div className="bg-[#020202]">
+    <main className="bg-[#020202] w-full min-h-screen">
       <Hero />
       <Perspective />
-      {/* This section needs room to breathe for the scroll to trigger */}
+      
+      {/* This creates the horizontal scroll area */}
       <SignatureCollection />
       
-      {/* Spacer to allow the end of the scroll to be felt */}
-      <div className="h-screen bg-[#020202]" />
-    </div>
+      {/* This creates the hover-reveal services area */}
+      <ServiceCanvas />
+
+      {/* Final Spacer for breathing room */}
+      <div className="h-[20vh] bg-[#020202]" />
+    </main>
   );
 }
 
