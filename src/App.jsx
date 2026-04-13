2e85f05a2e4c1516c0ba61dced67a35ba4b8d4cf
Animesh <unknowndevilcoder@gmail.com>
2026-04-13 11:48:36 +0530

    Completing Showcasing the collections in  Grey Shade Studios

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import Hero from './components/Hero';
 import Perspective from './components/Perspective';
-// import SignatureCollection from './components/SignatureCollection'; (Next step)
+import SignatureCollection from './components/SignatureCollection';
 
 function App() {
   return (
-    <main className="bg-[#020202]">
+    <div className="bg-[#020202]">
       <Hero />
       <Perspective />
-      {/* <SignatureCollection /> */}
-    </main>
+      {/* This section needs room to breathe for the scroll to trigger */}
+      <SignatureCollection />
+      
+      {/* Spacer to allow the end of the scroll to be felt */}
+      <div className="h-screen bg-[#020202]" />
+    </div>
   );
 }
 
