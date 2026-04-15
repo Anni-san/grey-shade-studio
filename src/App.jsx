f69ccd5b59affcaa04d1eb6a4201c0b2cf6ab215
Animesh <unknowndevilcoder@gmail.com>
2026-04-15 18:35:00 +0530

    Updating the App.jsx in Grey Shade Studios

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Perspective from './components/Perspective';
 import SignatureCollection from './components/SignatureCollection';
 import ServiceCanvas from './components/ServiceCanvas';
 import Process from './components/Process';
 import Footer from './components/Footer';
+import ClientPortal from './components/ClientPortal';
 
 function App() {
-  return (
-    <main className="bg-[#020202] w-full selection:bg-[#b09476] selection:text-white">
-      {/* 01. THE HOOK */}
-      <Hero />
-      
-      {/* 02. THE SOUL */}
-      <Perspective />
-      
-      {/* 03. THE WORK (Horizontal Scroll) */}
-      <SignatureCollection />
-      
-      {/* 04. THE CAPABILITIES */}
-      <ServiceCanvas />
-      
-      {/* 05. THE JOURNEY (Atmospheric Hover Reveal) */}
-      <Process />
+  // State to control which view we are in
+  const [isPortalOpen, setIsPortalOpen] = useState(false);
 
-      {/* 06. THE FINALE */}
-      <Footer />
+  return (
+    <main className="bg-[#020202] w-full min-h-screen selection:bg-[#b09476] selection:text-white">
+      
+      {/* CONDITIONAL RENDERING: If portal is open, show portal. Else, show storefront. */}
+      {isPortalOpen ? (
+        <ClientPortal onSignOut={() => setIsPortalOpen(false)} />
+      ) : (
+        <>
+          {/* THE INVISIBLE NAV */}
+          <Navbar onLoginClick={() => setIsPortalOpen(true)} />
+
+          {/* 01. THE HOOK */}
+          <Hero />
+          
+          {/* 02. THE SOUL */}
+          <Perspective />
+          
+          {/* 03. THE WORK (Horizontal Scroll) */}
+          <SignatureCollection />
+          
+          {/* 04. THE CAPABILITIES */}
+          <ServiceCanvas />
+          
+          {/* 05. THE JOURNEY (Atmospheric Hover Reveal) */}
+          <Process />
+
+          {/* 06. THE FINALE */}
+          <Footer />
+        </>
+      )}
     </main>
   );
 }
